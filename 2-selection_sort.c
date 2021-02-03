966c44d8187976cc2bcaa4279798066a236a4a06
musakanneh <mkanneh17@alustudent.com>
2021-02-03 21:14:11 +0200

    Qucik fix: betty styles

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,41 +7,41 @@
  */
 void swap(int *a, int *b)
 {
-    int tmp = *a;
+	int tmp = *a;
 
-    *a = *b;
-    *b = tmp;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
-* selection_sort - implementing the selection sort
-* @array: the array to be sorted
-* @size:size of the array
+ * selection_sort - implementing the selection sort
+ * @array: the array to be sorted
+ * @size:size of the array
  */
 void selection_sort(int *array, size_t size)
 {
-    /**
-     * look for the smallest element
-     * swap the smallest element with the current
-    */
-    unsigned int i = 0, j = 0, tmp_loc = 0;
-    int tmp;
+	/**
+	 * look for the smallest element
+	 * swap the smallest element with the current
+	 */
+	unsigned int i = 0, j = 0, tmp_loc = 0;
+	int tmp;
 
-    while (i < size)
-    {
-        j = i;
-        tmp = array[j], tmp_loc = j;
-        while (j < size)
-        {
-            if (array[j] < tmp)
-                tmp = array[j], tmp_loc = j;
-            j++;
-        }
-        if (array[i] != array[tmp_loc])
-        {
-            swap(array + i, array + tmp_loc);
-            print_array(array, size);
-        }
-        i++;
-    }
+	while (i < size)
+	{
+		j = i;
+		tmp = array[j], tmp_loc = j;
+		while (j < size)
+		{
+			if (array[j] < tmp)
+				tmp = array[j], tmp_loc = j;
+			j++;
+		}
+		if (array[i] != array[tmp_loc])
+		{
+			swap(array + i, array + tmp_loc);
+			print_array(array, size);
+		}
+		i++;
+	}
 }
